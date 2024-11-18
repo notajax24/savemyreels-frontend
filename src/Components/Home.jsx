2a0fe14b3261fe2b30bfcfb0b368d8f57332ce9c
notajax24 <96714712+notajax24@users.noreply.github.com>
2024-11-18 19:46:54 +0530

    ui changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -12,10 +12,11 @@ const Home = () => {
     videoUrl: null,
     thumbnail: null,
   });
+  const [newVideoUrl, setNewVideoUrl] = useState(null);
   const [loading, setLoading] = useState(false); // Track loading state
   const [progress, setProgress] = useState(0);
 
-  const BACKEND_URL = "http://localhost:5000";
+  const BACKEND_URL = "https://insta-video-downloader-backend.vercel.app:5000";
 
   // Function to handle paste button click
   const pasteFromClipboard = async () => {
@@ -35,16 +36,10 @@ const Home = () => {
         `${BACKEND_URL}/api/download?url=${encodeURIComponent(url)}`
       );
 
-      // console.log(response.data.videoUrl);
-
+      console.log(response.data.videoUrl);
+      setNewVideoUrl(response.data.videoUrl);
+      // setVideoData(response.data);
       // Check if videoUrl and thumbnail are present in the response
-      const { videoUrl, thumbnail } = response.data;
-
-      if (videoUrl && thumbnail) {
-        setVideoData({ videoUrl: videoUrl, thumbnail: thumbnail });
-      } else {
-        setError("No video URL or thumbnail found.");
-      }
     } catch (err) {
       setError("Failed to fetch video. Please check the URL.");
       console.error("Error fetching video:", err);
@@ -54,9 +49,10 @@ const Home = () => {
   };
 
   const downloadVideo = () => {
-    if (videoData && videoData.videoUrl) {
+    console.log(newVideoUrl);
+    if (newVideoUrl) {
       // Use FileSaver.js to download the video with a prompt for file name and location
-      saveAs(videoData.videoUrl, "savemyreelsapp.mp4");
+      saveAs(newVideoUrl, "savemyreelsapp.mp4");
     }
   };
 
@@ -117,7 +113,7 @@ const Home = () => {
 
           {error && <p className="text-red-500 mt-4 text-center">{error}</p>}
 
-          {videoData.videoUrl && (
+          {newVideoUrl && (
             <div className="mt-8 w-full max-w-2xl mx-auto px-4">
               <h2 className="text-xl font-semibold text-center mb-4">
                 Video Preview
@@ -127,15 +123,17 @@ const Home = () => {
                   controls
                   className="flex w-[400px] h-[600px] object-cover rounded-lg"
                 >
-                  <source src={videoData.videoUrl} type="video/mp4" />
+                  <source src={newVideoUrl} type="video/mp4" />
                   Your browser does not support the video tag.
                 </video>
               </div>
               <p className="text-center mt-4">
                 <button
                   onClick={downloadVideo}
+                  disabled={loading}
                   className="bg-blue-500 text-white py-2 px-4 rounded-lg shadow-md hover:bg-blue-700 w-full block text-center"
                 >
+                  {console.log(videoData.videoUrl)}
                   Download Video
                 </button>
               </p>
